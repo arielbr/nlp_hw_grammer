34b1c4fa0be123a9a421dab52539f2fa28f919e0
Ariel Bao <rbao4@jhu.edu>
2021-09-04 22:27:54 -0400

    moved randsent2 to randsent

--- randsent.py
+++ randsent.py
@@ -2,9 +2,11 @@
 """
 601.465/665 — Natural Language Processing
 Assignment 1: Designing Context-Free Grammars
+
 Assignment written by Jason Eisner
 Modified by Kevin Duh
 Re-modified by Alexandra DeLucia
+
 Code template written by Alexandra DeLucia,
 based on the submitted assignment with Keith Harrigian
 and Carlos Aguirre Fall 2019
@@ -13,6 +15,9 @@ import os
 import sys
 import random
 import argparse
+import pdb
+import re
+from collections import defaultdict
 
 # Want to know what command-line arguments a program allows?
 # Commonly you can ask by passing it the --help option, like this:
@@ -30,6 +35,7 @@ import argparse
 def parse_args():
     """
     Parse command-line arguments.
+
     Returns:
         args (an argparse.Namespace): Stores command-line attributes
     """
@@ -76,11 +82,29 @@ def parse_args():
     )
     return parser.parse_args()
 
+class Node(object):
+    
+    def __init__(self,name):
+        self.name = name
+        
+        self.parent = None
+        self.children = []
+        self.explored = False
+        self.isterminal = False
+        if self.name == 'ROOT':
+            self.isroot = True
+        else:
+            self.isroot = False
+            
+    def add_children(self,node):
+            self.children.append(node)
+        
 
 class Grammar:
     def __init__(self, grammar_file):
         """
         Context-Free Grammar (CFG) Sentence Generator
+
         Args:
             grammar_file (str): Path to a .gr grammar file
         
@@ -92,10 +116,12 @@ class Grammar:
         self.nonterminals = set()
         self.sum_dict = {}
         self._load_rules_from_file(grammar_file)
+        self.sampled_rules = defaultdict()
 
     def _load_rules_from_file(self, grammar_file):
         """
         Read grammar file and store its rules in self.rules
+
         Args:
             grammar_file (str): Path to the raw grammar file 
         """
@@ -115,40 +141,37 @@ class Grammar:
         for word in left_hand_side:
             if word not in right_hand_side:
                 self.nonterminals.add(word)
-
+        
         with open(grammar_file) as grammar_file2:
             for line in grammar_file2:
                 line = line.partition("#")[0]
                 line = line.strip()
                 if not line or line[0] == "#":
                     continue
+                #pdb.set_trace()
                 elements = line.split("\t")
+                 
                 if elements[1] in self.sum_dict.keys():
-                    self.sum_dict[elements[1]] += int(elements[0])
+                    # I think we should use counts here instead of adding probs
+                    self.sum_dict[elements[1]] += float(elements[0])
                 else:
-                    self.sum_dict[elements[1]] = int(elements[0])
+                    self.sum_dict[elements[1]] = float(elements[0])
                 
                 # adding dash around non-terminals for quicker split later
                 words = elements[2].split(" ")
                 for i in range(len(words)):
                     if words[i] in self.nonterminals:
-                        words[i] = "/" + words[i] + "/"
+                        words[i] = "/" + words[i] + "/"        
                 elements[2] = " ".join(words)
                 if elements[1] in self.rules:
-                    # for easier comparison later, each last element within a tuple represent total odds cumulatively
-                    # so instead of showing [(first_expansion, 1), (first_expansion, 2)], with total expansion odds = 3
-                    # self.rules[key] will show such a list: [(first_expansion, 1), (first_expansion, 1+2)]
-                    cumulative_freq = int(elements[0]) + self.rules[elements[1]][-1][-1]
-                    self.rules[elements[1]].append(tuple([elements[2], cumulative_freq]))
+                    self.rules[elements[1]][elements[2]] = float(elements[0])
                 else:
-                    cumulative_freq = int(elements[0])
-                    self.rules[elements[1]] = [tuple([elements[2], cumulative_freq])]
-        print(self.sum_dict)
-        print(self.rules)
+                    self.rules[elements[1]] = {elements[2]: float(elements[0])}
 
     def sample(self, derivation_tree, max_expansions):
         """
         Sample a random sentence from this grammar
+
         Args:
             derivation_tree (bool): if true, the returned string will represent 
                 the tree (using bracket notation) that records how the sentence 
@@ -159,7 +182,55 @@ class Grammar:
         Returns:
             str: the random sentence or its derivation tree
         """
-        raise NotImplementedError
+        self.traverse_output = ""
+
+        # depth-first expantion
+        self.root = Node("ROOT") # starting node
+        self.root.explored = True
+        self.traverse(self.root, max_expansions)
+        return self.traverse_output
+
+    # recursive function to help traversing 
+    def traverse(self, node, remaining_expansions):
+        remaining_expansions -= 1
+        if remaining_expansions <= 0:
+            self.traverse_output = self.traverse_output + "..." + ") "
+        else: 
+            if len(self.traverse_output) > 0 and self.traverse_output[-1] == ")":
+                self.traverse_output += " "
+            self.traverse_output = self.traverse_output + "(" + node.name + " "
+            # select one expansion rule by relative odds
+            choice_options = []
+            weights = []
+            for elements in self.rules[node.name].keys():
+                weights.append(self.rules[node.name][elements])
+                choice_options.append(elements)
+            sample = random.choices(choice_options, weights=weights, k=1)[0]
+
+            splitted = sample.split("/")
+            for i in range(len(splitted)):
+                child = splitted[i]
+                if child == "" or child == " ":
+                    continue
+                child = child.strip(" ")
+                child_node = Node(child)
+                if child in self.nonterminals:
+                    child_node.isterminal = False
+                    node.add_children(child_node)
+                    child_node.parent = node
+                    self.traverse(child_node, remaining_expansions) 
+                else:
+                    child_node.isterminal = True
+                    node.add_children(child_node)
+                    child_node.parent = node
+                    if self.traverse_output[-1] == ")":
+                        self.traverse_output += " "
+                    if i == len(splitted) - 1:
+                        self.traverse_output = self.traverse_output + child_node.name + ")"
+                        return
+                    else:
+                        self.traverse_output = self.traverse_output + child_node.name + " "
+            self.traverse_output += ")"
 
 
 ####################
